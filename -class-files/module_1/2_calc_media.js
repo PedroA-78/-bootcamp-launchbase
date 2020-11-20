a2683ebd1cc834e5f636d1b31cd5774616ee7669
PedroA-78 <67925461+PedroA-78@users.noreply.github.com>
2020-11-20 14:57:54 -0300

    File 2 - Calc Média

---
+++ -class-files/module_1/2_calc_media.js
@@ -0,0 +1,15 @@
+// Criar um programa que cálcula média
+// das notas entre os alunos e envia
+// mensagem do cálculo da média.
+
+const aluno1 = "Pandora"
+const aluno2 = "Pedro"
+const aluno3 = "Alice"
+
+const notaAluno1 = 10
+const notaAluno2 = 9
+const notaAluno3 = 9.7
+
+const media = (notaAluno1 + notaAluno2 + notaAluno3) / 3
+
+console.log(media.toFixed(1))
