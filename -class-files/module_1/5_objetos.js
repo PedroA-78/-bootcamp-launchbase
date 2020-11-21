ba56f849f258c59fbc07c1d22411d8511402899e
PedroA-78 <67925461+PedroA-78@users.noreply.github.com>
2020-11-20 22:08:23 -0300

    File 5 - Objetos

---
+++ -class-files/module_1/5_objetos.js
@@ -0,0 +1,18 @@
+const aluno1 = {
+    nome: "Pandora",
+    nota: 9.8
+}
+
+const aluno2 = {
+    nome: "Pedro",
+    nota: 8.6
+}
+
+const aluno3 = {
+    nome: "Alice",
+    nota: 9.4
+}
+
+const media = (aluno1.nota + aluno3.nota + aluno3.nota) / 3
+
+console.log(media)
