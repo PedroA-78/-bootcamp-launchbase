835c061dbd12e98a2f5f128a745f1fcfc1694482
PedroA-78 <67925461+PedroA-78@users.noreply.github.com>
2020-11-25 20:40:21 -0300

    Module 3 - Usando dados dinâmicos

--- -class-files/module_3/server.js
+++ -class-files/module_3/server.js
@@ -2,6 +2,7 @@ const express = require('express') // O require é usado quando nós importamos 
 const nunjucks = require('nunjucks') // Fazendo a requisição do nunjucks no nosso código
 
 const server = express() // Aqui a const express se tornou um função
+const videos = require("./data")
 
 server.use(express.static('public'))  // .use é Middleware. Middleware é um bloco de código que é executado em todas as requisições ou nas que respeitam um certo padrão. E é respeitado a ordem de adição de cada um deles.
 
@@ -9,15 +10,29 @@ server.set('view engine', 'njk') // .set é um método que define o valor da con
 
 nunjucks.configure('views', {
     express: server,
+    autoescape: false,
     noCache: true
 })
 
 server.get("/", function(req, res){ // .get "é um método que vai pegar rota estipulada e executar uma função com parâmetro de request do usuario e um parâmetro de resposta do servidor"
-    return res.render("about") // .render é um método usado para renderizar uma visualização e enviar a string HTML renderizada para o cliente.
+    const data = {
+        avatar_url: "/assets/fred_flintstone.png",
+        name: "Fred Flintstone",
+        role: "Operador de guindaste - Slate Rock and Gravel Company",
+        description: 'Fred é um típico operário, que trabalha como "operador de guindaste" na <a href="https://flintstones.fandom.com/wiki/Slate_Rock_and_Gravel_Company" target="_blank">Slate Rock and Gravel Company</a>.',
+        links: [
+            { name: "GitHub", url: "https://github.com/" },
+            { name: "Instagram", url: "https://www.instagram.com/" },
+            { name: "Twitter", url: "https://twitter.com/" }
+        ]
+
+    }
+
+    return res.render("about", {about: data}) // .render é um método usado para renderizar uma visualização e enviar a string HTML renderizada para o cliente.
 })
 
 server.get("/portifolio", function(req, res){
-    return res.render("portifolio")
+    return res.render("portifolio", {items: videos})
 })
 
 server.listen(5000, function(){ // .listen é um método agora do (server) que vai ficar "ouvido algo e quando esse algo acontecer ele executa uma função de callback, ou seja vai executar algo"
